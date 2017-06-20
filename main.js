03cd34cb72bb7a223a8dbaef1552670160cbe23c
Balaji Balakrishnan <balajibalakrishnan@balajis-air.lan>
2017-06-20 16:02:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const dataFile = require('./data.js');
+const express = require('express');
+const mustacheExpress = require('mustache-express');
+const app = express();
+
+app.use(express.static('public'));
+app.engine('mustache', mustacheExpress());
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.get('/',function(req, res){
+res.render('main', dataFile);
+});
+
+app.listen(3000, function () {
+console.log('Successfully started express application!');
+})
