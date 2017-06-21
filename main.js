d8a35306d3adcb8e880f3892614d9c7793dfd07e
Balaji Balakrishnan <balajibalakrishnan@Balajis-MacBook-Air.local>
2017-06-20 23:44:43 -0400

    incorporated review comments

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 
 app.use(express.static('public'));
+
 app.engine('mustache', mustacheExpress());
 app.set('views', './views')
 app.set('view engine', 'mustache')
