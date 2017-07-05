6c1ca9a8e634ec8c2eec02f0a175f3f56cf9f565
Balaji Balakrishnan <balajibalakrishnan@balajis-air.lan>
2017-07-05 16:30:25 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const dataFile = require('./data.js');
 const express = require('express');
 const mustacheExpress = require('mustache-express');
+var mongoClient = require('mongodb').MongoClient;
 const app = express();
 
 app.use(express.static('public'));
@@ -10,7 +11,15 @@ app.set('views', './views')
 app.set('view engine', 'mustache')
 
 app.get('/',function(req, res){
-res.render('main', dataFile);
+  mongoClient.connect("mongodb://127.0.0.1:27017/userdb", function(err, db) {
+    if(err) { return console.dir(err); }
+    else{
+        var dbCollection = db.collection('user');
+        dbCollection.find().toArray().then(function(user){
+          res.render('main', {user: user});
+        });
+      }
+  });
 });
 
 app.listen(3000, function () {
